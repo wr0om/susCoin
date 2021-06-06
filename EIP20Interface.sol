d8f893f8b804a41eaeb439636b79cd91e8a52a60
wrom <59180254+wr0om@users.noreply.github.com>
2021-06-06 17:53:37 +0300

    Create EIP20Interface.sol

---
+++ EIP20Interface.sol
@@ -0,0 +1,24 @@
+pragma solidity ^0.4.21;
+
+
+contract EIP20Interface {
+
+    /// total amount of tokens
+    uint256 public totalSupply;
+
+    /// @param _owner The address from which the balance will be retrieved
+    /// @return The balance
+    function balanceOf(address _owner) public view returns (uint256 balance);
+   
+    function transfer(address _to, uint256 _value) public returns (bool success);
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success);
+
+    function approve(address _spender, uint256 _value) public returns (bool success);
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining);
+
+    // solhint-disable-next-line no-simple-event-func-name
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
