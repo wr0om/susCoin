5e3eeae0b39ca586f6000b7f6129c5ae85a37f94
wrom <59180254+wr0om@users.noreply.github.com>
2021-06-06 18:08:52 +0300

    Update EIP20Interface.sol

--- EIP20Interface.sol
+++ EIP20Interface.sol
@@ -3,11 +3,8 @@ pragma solidity ^0.4.21;
 
 contract EIP20Interface {
 
-    /// total amount of tokens
     uint256 public totalSupply;
 
-    /// @param _owner The address from which the balance will be retrieved
-    /// @return The balance
     function balanceOf(address _owner) public view returns (uint256 balance);
    
     function transfer(address _to, uint256 _value) public returns (bool success);
